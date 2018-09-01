5dcc8e475b2e93cd8acbe840fdd6b0148dcc306b
Natasha Petrus <natashatheo@hotmail.com>
2018-08-31 17:43:44 -0700

    bundling js

---
+++ js/bundle.js
@@ -0,0 +1,9 @@
+---
+---
+{% include_relative bootstrap.min.js %}
+{% include_relative circle-progress.min.js %}
+{% include_relative jquery-2.1.4.min.js %}
+{% include_relative magnific-popup.min.js %}
+{% include_relative main.js %}
+{% include_relative mo.min.js %}
+{% include_relative owl.carousel.min.js %}
